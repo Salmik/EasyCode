e5aeedfa857a3dd74d237fe55b8f8a291190c62d
Zhanibek Lukpanov <janibek3394@mail.ru>
2024-07-02 15:25:23 +0500

    Initial Commit

---
+++ EasyCode/EasyCode.h
@@ -0,0 +1,18 @@
+//
+//  EasyCode.h
+//  EasyCode
+//
+//  Created by Zhanibek Lukpanov on 02.07.2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EasyCode.
+FOUNDATION_EXPORT double EasyCodeVersionNumber;
+
+//! Project version string for EasyCode.
+FOUNDATION_EXPORT const unsigned char EasyCodeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EasyCode/PublicHeader.h>
+
+
